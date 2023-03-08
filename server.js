6e41eaa278c05e2743b3e6f172a770bebeb1fc11
YoungTamm <noppakhon.c64@rsu.ac.th>
2023-03-08 16:07:55 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+  host: '157.245.59.56',
+  user: 'u6402249',
+  password: '6402249',
+  database: 'u6402249_Electric_Furniture',
+  port: 3366
+})
+
+var app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', function(req, res) {
+  res.json({
+    "status": "ok",
+    "message": "Hello World"
+  })
+})
+
+app.get('/customer', function(req, res) {
+  connection.query(
+    'SELECT * FROM a1_customer',
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
+
+app.get('/order', function(req, res) {
+  connection.query(
+    'SELECT * FROM a1_order',
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
+
+app.get('/product', function(req, res) {
+  connection.query(
+    'SELECT * FROM a1_product',
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
+
+app.listen(5000, () => {
+  console.log('Server is started.')
+})
+
